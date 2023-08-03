34bb4036d31a932e7b882083de04f6567c159c21
Shahiraosama <120308697+Shahiraosama@users.noreply.github.com>
2023-08-03 21:47:35 +0300

    Add files via upload

---
+++ Automatic_Garage_Door_Controller.v
@@ -0,0 +1,146 @@
+module Automatic_Garage_Door_Controller(
+input UP_Max,DN_Max,Activate,
+input clk,rst,
+output reg UP_M,DN_M
+);
+
+reg [1:0] state_reg,state_next;
+localparam IDLE=2'b00;
+localparam Mv_Dn=2'b01;
+localparam Mv_Up=2'b10;
+
+
+always@(posedge clk or negedge rst)
+
+begin
+
+      if(!rst)
+begin
+      state_reg<=IDLE;
+end
+       else
+begin
+      state_reg<=state_next;
+end
+
+end
+
+always@(*)
+begin
+       case(state_reg)
+IDLE:
+begin
+
+       if (Activate && UP_Max && !DN_Max)
+begin
+
+       state_next=Mv_Dn;
+
+end
+
+
+       else if (Activate && !UP_Max && DN_Max )
+begin
+
+      state_next=Mv_Up;
+end
+
+
+
+       else
+
+begin
+
+      state_next=IDLE;
+
+end
+
+
+end
+
+
+Mv_Dn:
+
+begin
+
+       if (DN_Max)
+begin
+
+      state_next=IDLE;
+
+end
+
+       else
+begin
+
+       state_next=Mv_Dn;
+end
+
+end
+
+Mv_Up:
+
+begin
+         if(UP_Max)
+begin
+       state_next=IDLE;
+end
+           else
+begin
+      state_next=Mv_Up;
+end
+
+end
+
+        default:state_next=IDLE;
+endcase
+
+end
+
+always@(*)
+begin
+        case(state_reg)
+IDLE:
+
+begin
+   
+UP_M=1'b0;
+DN_M=1'b0;
+
+end
+
+Mv_Dn:
+
+begin
+
+UP_M=1'b0;
+DN_M=1'b1;
+
+end
+
+Mv_Up:
+
+begin
+
+UP_M=1'b1;
+DN_M=1'b0;
+
+end
+
+default:
+
+begin
+
+UP_M  = 1'b0 ;
+DN_M  = 1'b0 ;
+
+end
+
+endcase
+
+end
+
+
+
+
+endmodule
---
+++ Automatic_Garage_Door_Controller_TB.v
@@ -0,0 +1,94 @@
+`timescale 1ns/1ps
+module Automatic_Garage_Door_Controller_TB ();
+reg clk_tb;
+reg rst_tb;
+reg UP_Max_tb;
+reg DN_Max_tb;
+reg Activate_tb;
+wire UP_M_tb;
+wire DN_M_tb;
+
+localparam T=20;
+
+always
+begin
+#(T/2) clk_tb=~clk_tb;
+end
+
+Automatic_Garage_Door_Controller DUT(
+.clk(clk_tb),
+.rst(rst_tb),
+.UP_Max(UP_Max_tb),
+.DN_Max(DN_Max_tb),
+.Activate(Activate_tb),
+.UP_M(UP_M_tb),
+.DN_M(DN_M_tb)
+);
+
+initial 
+begin
+$dumpfile ("Garage_DOOr_Controller_dump.vcd");
+$dumpvars;
+
+initialize();
+
+reset();
+
+Activate();
+
+UP_MAX();
+
+Activate();
+
+DN_MAX();
+
+#100 $finish;
+
+end
+
+task initialize;
+begin
+clk_tb=1'b0;
+Activate_tb=1'b0;
+UP_Max_tb=1'b0;
+DN_Max_tb=1'b1;
+end
+endtask
+
+
+task reset;
+begin
+ rst_tb =  1'b1;
+ #1
+ rst_tb  = 1'b0;
+ #1
+ rst_tb  = 1'b1;
+ 
+end
+endtask
+
+
+task UP_MAX;
+begin
+#(5*T) UP_Max_tb=1'b1; DN_Max_tb=1'b0;
+end
+endtask
+
+task Activate;
+begin
+Activate_tb=1'b0;
+#(3*T) 
+Activate_tb=1'b1;
+end
+endtask
+
+
+task DN_MAX;
+begin
+#(5*T) UP_Max_tb=1'b0; DN_Max_tb=1'b1;
+end
+
+endtask
+
+
+endmodule
